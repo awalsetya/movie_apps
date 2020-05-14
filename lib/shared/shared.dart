c73207003112233f2f300b397aed2a3c93b54de7
awal <awalsetyanto.1993@gmail.com>
2020-05-14 11:29:50 +0700

    update splash page

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_apps/bloc/blocs.dart';
 
 part 'shared_value.dart';
+part 'theme.dart';
